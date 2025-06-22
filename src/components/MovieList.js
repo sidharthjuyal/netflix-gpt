f0641e28c256ba170aed68cf375e5c81d42bcad3
sidharthjuyal <sid.juyal.34@gmail.com>
2025-06-23 03:49:04 +0530

    final commit V3
    final commit V3

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,14 +1,18 @@
 import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, movies }) => {
+  if (!movies || movies.length === 0) return null;
+
   return (
     <div className="px-9 text-white">
-      <h1 className="text-lg md:text-2xl text-bold py-6 text-center md:text-left">{title}</h1>
+      <h1 className="text-lg md:text-2xl text-bold py-6 text-center md:text-left">
+        {title}
+      </h1>
       <div className="flex overflowing-div overflow-x-scroll">
         <div className="flex">
-          {movies?.map((movie) => {
-            return <MovieCard key={movie.id} movie={movie} />;
-          })}
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
         </div>
       </div>
     </div>
