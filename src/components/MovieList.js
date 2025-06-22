8b30bb2a30c0dcb985163ba4bfae16027feadbc6
sidharthjuyal <sid.juyal.34@gmail.com>
2025-06-23 04:57:57 +0530

    Project End Commit
    Project End Commit

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,12 +1,14 @@
 import MovieCard from "./MovieCard";
+import { InfoCircle } from "react-bootstrap-icons";
 
 const MovieList = ({ title, movies }) => {
   if (!movies || movies.length === 0) return null;
 
   return (
     <div className="px-9 text-white">
-      <h1 className="text-lg md:text-2xl text-bold py-6 text-center md:text-left">
+      <h1 className="text-lg md:text-2xl font-bold py-6 flex items-center text-center md:text-left">
         {title}
+        <InfoCircle color="white" size={16} className="ml-2 cursor-pointer mt-1" title={"This section contains movies with similar name as: " + title} />
       </h1>
       <div className="flex overflowing-div overflow-x-scroll">
         <div className="flex">
