5b3e8ef73334c61f229635277b7c16a4b56215d2
sidharthjuyal <sid.juyal.34@gmail.com>
2025-06-23 00:52:27 +0530

    minor fixes
    minor fixes

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -5,42 +5,27 @@ import { addTrailerVideo } from "../utils/moviesSlice";
 
 const useMovieTrailer = (movieId) => {
   const dispatch = useDispatch();
-  const trailerVideo = useSelector((store) => store.movies.trailerVideo);
+  const trailerVideo = useSelector(store => store.movies.trailerVideo);
 
   const getMovieVideo = async () => {
-    try {
-      const res = await fetch(
-        `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
-        API_OPTIONS
-      );
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/" +
+        movieId +
+        "/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    const filterData = json.results.filter(
+      (video) => video.name == "Official Teaser Trailer"
+    );
+    const trailer = filterData.length ? filterData[0] : json.results[0];
 
-      if (!res.ok) {
-        console.error(
-          `Failed to fetch trailer for movie ID ${movieId}: ${res.status} ${res.statusText}`
-        );
-        return;
-      }
-
-      const json = await res.json();
-
-      const filterData = json.results?.filter(
-        (video) => video.name === "Official Teaser Trailer"
-      );
-      const trailer = filterData?.length ? filterData[0] : json.results?.[0];
-
-      if (trailer) {
-        dispatch(addTrailerVideo(trailer));
-      } else {
-        console.warn(`No trailer found for movie ID ${movieId}`);
-      }
-    } catch (error) {
-      console.error(`Error fetching trailer for movie ID ${movieId}:`, error);
-    }
+    dispatch(addTrailerVideo(trailer));
   };
 
   useEffect(() => {
-    if (!trailerVideo && movieId) getMovieVideo();
-  }, [trailerVideo, movieId]);
+    !trailerVideo && getMovieVideo();
+  }, [trailerVideo]);
 };
 
 export default useMovieTrailer;
