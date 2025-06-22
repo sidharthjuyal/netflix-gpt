ed83f5a83dbe4f72fa811785563ec05fead500e0
sidharthjuyal <sid.juyal.34@gmail.com>
2025-06-23 05:06:02 +0530

    Project End Commit
    Project End Commit

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -10,7 +10,7 @@ const MovieList = ({ title, movies }) => {
     <div className="px-9 text-white">
       <h1 className="text-lg md:text-2xl font-bold py-6 flex items-center text-center md:text-left">
         <span>{title}</span>
-        {!showGptSearch && <span><InfoCircle color="white" size={16} className="ml-2 cursor-pointer mt-1" title={"This section contains movies with similar name as: " + title} /></span>}
+        {showGptSearch && <span><InfoCircle color="white" size={16} className="ml-2 cursor-pointer mt-1" title={"This section contains movies with similar name as: " + title} /></span>}
       </h1>
       <div className="flex overflowing-div overflow-x-scroll">
         <div className="flex">
